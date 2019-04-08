5a55060088d4e47fddbce055d159308266c98d07
Ryan Weaver <ryan@thatsquality.com>
2019-04-08 15:11:51 -0400

    making service more interesting

--- modules/custom/custom/coffee_shop/src/Service/Barista.php
+++ modules/custom/custom/coffee_shop/src/Service/Barista.php
@@ -2,8 +2,19 @@
 
 namespace Drupal\coffee_shop\Service;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
+
 class Barista {
+  private $configFactory;
+
+  public function __construct(ConfigFactoryInterface $configFactory) {
+    $this->configFactory = $configFactory;
+  }
+
   public function prepareDrink($type) {
+    // pretend like I'm using the configFactory
+    //$value = $this->configFactory->get('some_form_name')->get('some_field');
+
     $sizes = ['small', 'medium', 'large', 'scary huge'];
     $statuses = ['for here', 'to go', 'to sip while standing pensively'];
 
