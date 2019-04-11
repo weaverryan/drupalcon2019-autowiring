c38e07d3532ef98696256e169adf6ba93451471c
Ryan Weaver <ryan@thatsquality.com>
2019-04-10 17:08:44 -0700

    Step 7: Register: Auto-register services

--- modules/custom/custom/coffee_shop/src/CoffeeShopServiceProvider.php
+++ modules/custom/custom/coffee_shop/src/CoffeeShopServiceProvider.php
@@ -4,15 +4,28 @@ namespace Drupal\coffee_shop;
 
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\DependencyInjection\ServiceProviderInterface;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\Finder\Finder;
 
 class CoffeeShopServiceProvider implements ServiceProviderInterface {
   public function register(ContainerBuilder $container) {
-    foreach ($container->getDefinitions() as $id => $definition) {
-      if (strpos($id, 'Drupal\\coffee_shop') !== 0) {
+    $finder = new Finder();
+    $finder->in(__DIR__.'/Service')
+      ->files()
+      ->name('*.php');
+
+    foreach ($finder as $fileInfo) {
+      $class = 'Drupal\coffee_shop\Service\\'
+        .substr($fileInfo->getFilename(), 0, -4);
+
+      // don't override any existing service
+      if ($container->hasDefinition($class)) {
         continue;
       }
 
+      $definition = new Definition($class);
       $definition->setAutowired(true);
+      $container->setDefinition($class, $definition);
     }
   }
 }
