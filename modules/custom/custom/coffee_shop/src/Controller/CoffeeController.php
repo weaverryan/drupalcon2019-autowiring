838145bc24d954375cf1db31e085be0e8a3094d7
Ryan Weaver <ryan@thatsquality.com>
2019-04-10 17:08:41 -0700

    Step 1: Service Id: use class name

--- modules/custom/custom/coffee_shop/src/Controller/CoffeeController.php
+++ modules/custom/custom/coffee_shop/src/Controller/CoffeeController.php
@@ -5,7 +5,7 @@ namespace Drupal\coffee_shop\Controller;
 class CoffeeController {
   public function brewCoffee($type) {
     $barista =  \Drupal::getContainer()
-      ->get('coffee_shop.barista');
+      ->get('Drupal\coffee_shop\Service\Barista');
     $text = $barista->prepareDrink($type);
 
     return [
